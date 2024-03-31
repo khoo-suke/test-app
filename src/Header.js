a098792f0bedd39b70f749360c715222ba700c70
kosuke <sasaki.kosuke8@gmail.com>
2024-03-31 18:26:40 +0900

    インデックスページリストの作成

---
+++ src/Header.js
@@ -0,0 +1,12 @@
+import './Header.css';
+
+export default function Header() {
+  return (
+    <>
+      <header className="header">
+        <a href="/">Blog</a>
+        <a href="/">お問い合わせ</a>
+    </header>
+    </>
+  )
+}
