085cb10451a3390b46f7b23cd75b652476f3c2fb
kosuke <sasaki.kosuke8@gmail.com>
2024-04-02 11:07:24 +0900

    課題3_初回コミット

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
 import { Route, createBrowserRouter, createRoutesFromElements } from 'react-router-dom';
 
 import Header from './Header';
-import ListPost from './ListPost';
+// import ListPost from './ListPost';
+import ListPostAPI from './ListPostAPI';
 import Contact from './Contact';
-import PostDitail from './PostDitail';
+// import PostDitail from './PostDitail';
+import PostDitailAPI from './PostDitailAPI';
 
 const App = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<Header/>}>
-      <Route path="/" element={<ListPost />} />
+      {/* <Route path="/" element={<ListPost />} /> */}
+      <Route path="/" element={<ListPostAPI />} />
       <Route path="contact" element={<Contact />} />
-      <Route path="post/:postId" element={<PostDitail/>} />
+      {/* <Route path="post/:postId" element={<PostDitail/>} /> */}
+      <Route path="post/:postId" element={<PostDitailAPI/>} />
     </Route>
   )
 );
